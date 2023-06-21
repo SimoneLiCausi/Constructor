221603754c0636f2120d818f93fe30276534526f
Simone Li Causi <simonelicausi96@gmail.com>
2023-06-21 16:10:26 +0200

    Initial commit

---
+++ src/Exercise.java
@@ -0,0 +1,19 @@
+
+//you declare two House variables house1 and house2
+//you create two House objects and assign them to the variables house1 and house2
+public class Exercise {
+
+    public static void main(String[] args) {
+
+        House house1 = new House("Via R.Candida", 3);
+        House house2 = new House("Via Manzoni", 2);
+
+
+    }
+
+
+
+
+
+
+}
